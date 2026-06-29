ccfd9788b48a7f45d4a190abf579c76cfaa3adb6
ritesh07k <rriteshkumar257@gmail.com>
2026-06-29 11:32:38 +0000

    phase 10: move @EnableJpaAuditing, @EnableAsync, @EnableScheduling to dedicated config classes

---
+++ payUp/src/main/java/com/payUp/build/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.payUp.build.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
